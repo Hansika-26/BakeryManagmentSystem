32f90edc4fa508195e4515d8ca2f2fd3c5545b0a
Hansika-26 <IT23140998@my.sliit.lk>
2025-06-25 00:28:11 +0530

    Implemented email service and OTP verification for auth flow

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ import cookieParser from "cookie-parser";
 import connectDB from './config/mongodb.js';
 import authRouter from './routes/authRoutes.js';
 
+
+
 const app = express();
 const port = process.env.PORT || 4000
 connectDB();
@@ -19,4 +21,6 @@ app.get('/', (req, res)=> res.send("API Working"));
 app.use('/api/auth', authRouter)
 
 app.listen(port, ()=> console.log(`Server started on PORT:${port}`));
+
+
 
